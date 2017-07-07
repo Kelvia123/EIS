e275ecc6505b9a30dbd35f89bb533dd7643df8c4
NingLuo <luo.n@husky.neu.edu>
2017-07-07 14:49:52 -0400

    Add Update Employee Profile Markup

--- EIS.AngularJSUI/eis.js
+++ EIS.AngularJSUI/eis.js
@@ -9,7 +9,7 @@ appEIS.config(function($routeProvider) {
     $routeProvider.when('/Home', { templateUrl: 'Views/Common/Home/Home.html', controller: 'homeController' });   
     $routeProvider.when('/RecoverPassword', { templateUrl: 'Views/Common/RecoverPassword/RecoverPassword.html', controller: 'recoverPasswordController' });
     $routeProvider.when('/EmployeeManagement', { templateUrl: 'Views/Employee/EmployeeMgmt/EmployeeMgmt.html', controller: 'employeeMgmtController' });
-    $routeProvider.when('/EmployeeProfile', { templateUrl: 'Views/Employee/EmployeeUpdate/EmployeeUpdate.html', controller: 'employeeUpdateController' });
+    $routeProvider.when('/EmployeeProfile/:EmployeeId?', { templateUrl: 'Views/Employee/EmployeeUpdate/EmployeeUpdate.html', controller: 'employeeUpdateController' });
     $routeProvider.when('/Login', { templateUrl: 'Views/Common/Login/Login.html', controller: 'loginController' });
     $routeProvider.when('/Logout', {  });
     $routeProvider.otherwise({ redirectTo: '/Home' });
