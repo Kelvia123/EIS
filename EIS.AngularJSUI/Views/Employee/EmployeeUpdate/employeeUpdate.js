e2cbcdac5084e7ab385751ffb9d26135240c42c1
NingLuo <luo.n@husky.neu.edu>
2017-07-07 16:20:30 -0400

    Implement Get Employee for Employee Update UI

--- EIS.AngularJSUI/Views/Employee/EmployeeUpdate/employeeUpdate.js
+++ EIS.AngularJSUI/Views/Employee/EmployeeUpdate/employeeUpdate.js
@@ -1,7 +1,32 @@
 ﻿(function (undefined) {
+    appEIS.factory('employeeUpdateService', function($http) {
+        var empUpdateObj = {};
 
-    appEIS.controller('employeeUpdateController', function ($scope) {
-        $scope.msg = 'Welcom to employeeUpdate';
+        empUpdateObj.getById = function (empId) {
+            var employee;
+
+            employee = $http({ method: 'Get', url: 'http://localhost:4676/api/Employee', params: { id: empId } })
+                        .then(function(response) { return response.data; });
+
+            return employee;
+        }
+
+        return empUpdateObj;
+    });
+
+    appEIS.controller('employeeUpdateController', function ($scope, $routeParams, employeeUpdateService) {
+
+        $('#profilePanel a').click(function (e) {
+            e.preventDefault();
+        });
+
+        $scope.eid = $routeParams.EmployeeId;
+        employeeUpdateService.getById($scope.eid)
+                             .then(function(result) {
+                                 $scope.Emp = result;
+                                 //For DOJ's proper presentation in date input
+                                 $scope.Emp.DateOfJoin = new Date($scope.Emp.DateOfJoin);
+                             });
     });
 
 })();
