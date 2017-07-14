ffacabfd93960f5fbbfe8f9610ded280bd257192
NingLuo <luo.n@husky.neu.edu>
2017-07-14 01:31:15 -0400

    Move Send Email Config Info to Web.config

--- EIS.BLL/Utility.cs
+++ EIS.BLL/Utility.cs
@@ -10,21 +10,12 @@ namespace EIS.BLL
     public class Utility
     {
         public static bool SendMail(string toEmail, string subject, string body)
-        {
-            var smtpClient = new SmtpClient
-            {
-                Port = 587,
-                Host = "smtp.gmail.com",
-                EnableSsl = true,  // Gmail uses https, so ssl is true
-                Timeout = 10000,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential("ningluo89@gmail.com", "my gmail password") // Change to my password here
-            };
-
+        {           
             var mailMessage = new MailMessage("ningluo89@gmail.com", toEmail, subject, body);
 
-            // Uncomment this to send email
+            var smtpClient = new SmtpClient();
+
+            // Uncomment this to send email, and change the password in Web.config
             //smtpClient.Send(mailMessage);
 
             return true;
